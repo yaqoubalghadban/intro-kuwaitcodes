11a6e75e5d7d8b6a423af68b357e82df98fb2b83
yaqoubalghadban <yoyob4654@gmail.com>
2022-03-02 19:52:18 +0300

    add my name

---
+++ hi-me/hi-me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hi-me
+//
+//  Created by Yaquob Alghadban  on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, yaqoub")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hi-me/hi-me/hi_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  hi_meApp.swift
+//  hi-me
+//
+//  Created by Yaquob Alghadban  on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct hi_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
